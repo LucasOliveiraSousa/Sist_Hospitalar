57f74056382b22b70f83b5950b434b24acc72578
Lucas Sousa <89668332+LucasOliveiraSousa@users.noreply.github.com>
2026-05-24 14:52:30 -0300

    Add SistHospitalarApplication main class

---
+++ src/main/java/com/example/sist_hospitalar/SistHospitalarApplication.java
@@ -0,0 +1,15 @@
+package com.example.sist_hospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Aplicação Principal - Sistema Hospitalar
+ */
+@SpringBootApplication
+public class SistHospitalarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistHospitalarApplication.class, args);
+    }
+}
